c7f7822e19cfe33a7edd00e7d9a5dd9e5d1dd7cc
jennieevans <jennie1evans@hotmail.com>
2022-08-07 23:01:04 +0100

    console logs location in post and get request

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
+const { application } = require('express');
 var express = require('express');
 var router = express.Router();
 
+let location = ""
 /* GET users listing. */
+router.post('/', function(req, res, next) {
+  location = req.body.value
+  console.log(location)
+  res.json(req.body.value);
+});
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  console.log(location)
+  res.send(location);
 });
 
 module.exports = router;
---
+++ spec/pollen.test.js
@@ -0,0 +1,36 @@
+const Pollen = require('../routes/pollen');
+
+// require('jest-fetch-mock').enableMocks()
+
+describe('API class', () => {
+  it ('calls fetch and loads data', () => {
+  const pollenResponse = new PollenResponse();
+  fetch.mockResponseOnce(JSON.stringify({
+      "Count": {
+        "grass_pollen":31,
+        "tree_pollen":0,
+        "weed_pollen":126
+      },
+      "Risk":{
+        "grass_pollen":"Moderate",
+        "tree_pollen":"Low",
+        "weed_pollen":"High"
+      },
+      "Species":{
+        "Grass":{"Grass / Poaceae":31},
+        "Others":4,
+        "Tree":{"Alder":0,"Birch":0,"Cypress":0,"Elm":0,"Hazel":0,"Oak":0,"Pine":0,"Plane":0,"Poplar / Cottonwood":0},
+        "Weed":{"Chenopod":1,"Mugwort":2,"Nettle":113,"Ragweed":6}
+      },
+      "updatedAt":"2022-08-04T13:47:35.000Z"
+    }
+  ))
+    expect(data.Count.grass_pollen).toBe(31)
+    expect(data.Count.Risk).toBe({
+      "grass_pollen":"Moderate",
+      "tree_pollen":"Low",
+      "weed_pollen":"High"
+    })
+    expect(data.updatedAt).toBe("2022-08-04T13:47:35.000Z")
+  })
+})

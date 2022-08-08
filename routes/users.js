4290d20c3087dc1cbfeea3b946d552a4b26e0a8e
fridayshoes <one_amp@hotmail.com>
2022-08-08 17:47:09 +0100

    Initial mongo DB setup, named databases and schema

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,6 @@ const { application } = require('express');
 var express = require('express');
 var router = express.Router();
 
-
-
-
 // let email = ""
 
 router.post('/', function(req, res, next) {
@@ -20,5 +17,23 @@ router.post('/', function(req, res, next) {
 
 
 /*Get Users email submission*/
+
+Create: (req, res) => {
+  const user = new User({
+    email: req.body.email
+  });
+  user.save((err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(409).render("users/new", { 
+        error: 'User already exists!', 
+        email: req.email});
+    } else if (result) {
+      res.status(201).redirect("/sessions/new");
+    }
+  });
+},
+
+
 
 module.exports = router;

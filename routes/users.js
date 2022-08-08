0261280356f9a0ca6d8db26500186949710d24ea
Ephron Agyeman-opoku <ephronagyeman-opoku@Ephrons-MacBook-Air.local>
2022-08-08 16:27:41 +0100

    updated users route to express email submissions into the console

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
+const { application } = require('express');
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+
+
+// let email = ""
+
+router.post('/', function(req, res, next) {
+  email = req.body.value
+  console.log(email)
+  res.json(req.body.value);
 });
+
+/* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send(email);
+// });
+
+
+/*Get Users email submission*/
 
 module.exports = router;

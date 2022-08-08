49a15fd34e8f0f35bbc4a6b7cfd764365fe5b8da
jennieevans <jennie1evans@hotmail.com>
2022-08-08 12:07:41 +0100

    api call with variable location data working

--- routes/pollen.js
+++ routes/pollen.js
@@ -2,22 +2,31 @@ var express = require('express');
 var axios = require("axios").default;
 var router = express.Router();
 
+// let location = ""
+// /* GET users listing. */
+// router.post('/', function(req, res, next) {
+//   const location = req.body.value
+//   console.log(location)
+//   res.json(req.body.value);
+// });
+
 /* GET pollen listing. */
-router.get('/', function(req, res, next) {
+router.post('/', function(req, res, next) {
   /* API to GET pollen by place. */
-var pollenPlace = {
-  method: 'GET',
-  url: 'https://api.ambeedata.com/latest/pollen/by-place',
-  params: {place: 'Chingford'},
-  headers: {'x-api-key': '5e552db9ddf4a0e307958dc2719ba5ea2f1e3de42e8f96b689799b5f221f7aef', 'Content-type': 'application/json'}
-};
-axios.request(pollenPlace).then(function (response) {
-	console.log("Pollen Place");
-  console.log(response.data.data[0].Risk);
-  res.send(response.data.data);
-}).catch(function (error) {
-	console.error(error);
-});
+  console.log(req.body)
+  var pollenPlace = {
+    method: 'GET',
+    url: 'https://api.ambeedata.com/latest/pollen/by-place',
+    params: {place: req.body.value },
+    headers: {'x-api-key': '5e552db9ddf4a0e307958dc2719ba5ea2f1e3de42e8f96b689799b5f221f7aef', 'Content-type': 'application/json'}
+  };
+  axios.request(pollenPlace).then(function (response) {
+    console.log("Pollen Place");
+    console.log(response.data.data);
+    res.send(response.data.data);
+  }).catch(function (error) {
+    console.error(error);
+  });
 });
 
 module.exports = router;

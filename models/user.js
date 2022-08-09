4807a3c3491ddb60dd286f0ad0e306670979d579
fridayshoes <one_amp@hotmail.com>
2022-08-09 23:16:23 +0100

    update to schema in user model, preparation for next stage

--- models/user.js
+++ models/user.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
   email: { type : String, unique: true, required : true, dropDups: true },
-// password: { type: String, required: true },
-//  name: {type: String, required: true}, 
-//  surname: {type: String, required: true},
+  // mobile_number: { type: String, required: true },
+  // location: { type: String, required: true },
+  // pollen_type: { type: String, required: true },
 });
 
 const User = mongoose.model("User", UserSchema);
